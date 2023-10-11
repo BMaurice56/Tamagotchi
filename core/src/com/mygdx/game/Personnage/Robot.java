fd3c149bf7bef0a5d370b136bab460e4ef0dfcc7
benjamin <haurognebenjamin@gmail.com>
2023-10-11 21:58:47 +0200

    Implementation des methodes restant sauf "repair()"

--- core/src/com/mygdx/game/Personnage/Robot.java
+++ core/src/com/mygdx/game/Personnage/Robot.java
@@ -16,8 +16,8 @@ public class Robot extends Tamagotchi{
     public Robot(int difficulty) {
         super(difficulty);
         battery = 1000;
-        maintenance = 1000;
         tank = 1000;
+        maintenance = 1000;
         durability = 1000;
         happiness = 1000;
 
@@ -44,16 +44,134 @@ public class Robot extends Tamagotchi{
 
     public void setHappiness(int happiness){ this.happiness = happiness; }
 
+    public void check() {
+        if (battery < 0) {
+            battery = 0;
+        }
+        if (tank < 0) {
+            tank = 0;
+        }
+        if (maintenance < 0) {
+            maintenance = 0;
+        }
+        if (durability < 0) {
+            durability = 0;
+        }
+        if (happiness < 0) {
+            happiness = 0;
+        }
+        if (tank > 1000){
+            tank = 1000;
+        }
+    }
+
     public void reload() throws InterruptedException{
 
         int interval = random.nextInt(4);
 
         TimeUnit.SECONDS.sleep(15 + interval);
 
-        if (battery <= 200){
-            setBattery(900 - getDifficulty() * 100);
+        if (durability <= 200){
+            setDurability(900 - getDifficulty() * 100);
         }else{
-            setBattery(1000);
+            setDurability(1000);
         }
+    }
+
+    public void updating() throws InterruptedException{
+
+        int interval = random.nextInt(6);
+
+        TimeUnit.SECONDS.sleep(12 + interval);
+
+        happiness += 100;
+
+        if (happiness > 1000){
+            setHappiness(1000);
+        }
+
+        if (maintenance <= 200){
+            setMaintenance(900 - getDifficulty() * 85);
+        }else{
+            setMaintenance(1000);
+        }
+    }
+
+    public void dance() throws InterruptedException {
+
+        int interval = random.nextInt(6);
+
+        TimeUnit.SECONDS.sleep(10 + interval);
+
+        maintenance -= random.nextInt(75, 200);
+
+        check();
+
+        if(100 <= happiness && happiness <= 200){
+            setHappiness(900 - getDifficulty() * 100);
+        } else if (happiness < 100) {
+            setHappiness(750 - getDifficulty() * 100);
+        }else {
+            setHappiness(1000);
+        }
+    }
+
+    public void fillTank(String tank) throws InterruptedException{
+
+        for (int i = 0; i < getBasket().size(); i++){
+
+            Food food1 = getBasket().get(i);
+
+            if (tank.equals(food1.getName())) {
+                removePanier(i);
+                tank += food1.getPoint();
+
+                TimeUnit.SECONDS.sleep(5);
+
+                if (tank.equals("SuperExtraOil")) {
+                    happiness += 75;
+                }
+                if (happiness > 1000){
+                    setHappiness(1000);
+                }
+                check();
+            }
+        }
+    }
+
+    //public void repair(){}
+
+    public void work() throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(12);
+
+        setWallet(getWallet() + 50);
+        happiness -= getHappiness() * 100;
+        maintenance -= getMaintenance() * 43;
+        durability -= getDurability() * 60;
+
+        check();
+    }
+
+    public void buyOil(){
+
+        Food f = new Oil();
+        if(getWallet() >= f.getPrice()){
+            addBasket(f);
+            setWallet(getWallet() - f.getPrice());
+        }
+    }
+
+    public void buySuperExtraOil(){
+
+        Food f = new SuperExtraOil();
+        if(getWallet() >= f.getPrice()){
+            addBasket(f);
+            setWallet(getWallet() - f.getPrice());
+        }
+    }
+
+    public void Afficher_Attribut() {
+        System.out.println("vie : " + battery + "\nfood : " + tank + "\nhygiene: " + maintenance + "\nsleep: " + durability + "\nbonheur : " + happiness + "\nwallet :" + this.getWallet());
     }
 }
